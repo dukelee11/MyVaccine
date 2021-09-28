f3a554242104499631e9ae1ef3a18b6be06a6cc2
Duke Lee <leeduke90@gmail.com>
2021-09-28 12:58:56 -0400

    finish webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,36 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: {
-    src: "./src/index.js",
+    src: './src/index.js',
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, 'dist')
-
-  }
-}
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/dist',
+    compress: true,
+    port: 8080,
+  },
+};
